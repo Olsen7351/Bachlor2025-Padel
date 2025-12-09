21c030d391cc68b07619c33c8351ef613873a222
Emil Brügge <equistgaard@yahoo.dk>
2025-12-09 15:13:40 +0100

    Untrack ignored files

--- ML-Models/Models/Player & ball hits detection/trackers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .player_tracker import PlayerTracker
-from .ball_tracker import BallTrackerYOLO
-from .rally_tracker import RallyTracker
-from .ball_tracker_tracknet import BallTrackerTrackNet
--- ML-Models/Models/Player & ball hits detection/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .video_utils import read_video, save_video
-from .config_utils import load_court_config
-from main_utils import calibrate_exclusion_zones
